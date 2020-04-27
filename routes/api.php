ea2f740d2f28bb967a720d104adf211b63e19eff
kabaiss <aissakabot@gmail.com>
2020-04-27 12:04:48 +0200

    handling like replies

--- routes/api.php
+++ routes/api.php
@@ -16,5 +16,7 @@ use Illuminate\Http\Request;
 Route::apiResource('/questions','QuestionController' );
 Route::apiResource('/categories','CategoryController' );
 Route::apiResource('/questions/{question}/reply','ReplyController' );
+Route::post('like/{reply}', 'LikeController@likeIt');
+Route::delete('like/{reply}', 'LikeController@unlikeIt');
  
 
