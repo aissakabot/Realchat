055f62f27642d9dcb5539424418b2b893a0f1d16
kabaiss <aissakabot@gmail.com>
2020-04-28 11:27:03 +0200

    add vuetify and configure vouter vue

---
+++ resources/js/components/AppHome.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+      <tool-bar></tool-bar>
+      <footer></footer>
+  </div>
+</template>
+
+<script>
+import ToolBar from './ToolBar'
+import Footer from './Footer'
+export default {
+components:{
+    ToolBar,Footer
+},
+}
+</script>
+
+<style>
+
+</style>
---
+++ resources/js/components/Footer.vue
@@ -0,0 +1,22 @@
+<template>
+  
+  <v-footer padless>
+    <v-col
+      class="text-center"
+      cols="12"
+    >
+      {{ new Date().getFullYear() }} — <strong>Real Time system</strong>
+    </v-col>
+  </v-footer>
+
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
